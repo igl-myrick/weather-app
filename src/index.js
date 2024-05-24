8275b2c0b8b159dc817022a17fec339415e16834
igl-myrick <igl.myrick@gmail.com>
2024-05-24 15:44:50 -0700

    update getWeather function to use promises

--- src/index.js
+++ src/index.js
@@ -5,33 +5,38 @@ import "./css/styles.css";
 // business Logic
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-  
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    request.addEventListener("loadend", function() {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, city]);
+      } else {
+        reject([this, response, city]);
+      }
+    });
+    request.open("GET", url, true);
+    request.send();
   });
 
-  request.open("GET", url, true);
-  request.send();
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
 }
 
 // ui logic
 
-function printError(request, apiResponse, city) {
-  document.querySelector("#show-response").innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(error) {
+  document.querySelector("#showResponse").innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
 
-function printElements(apiResponse, city) {
-  console.log(apiResponse);
-  document.querySelector("#show-response").innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Fahrenheit is ${((apiResponse.main.temp - 273.15) * 9/5 + 32).toFixed(2)} degrees.
-  The weather is ${apiResponse.weather[0].description}.`;
+function printElements(data) {
+  document.querySelector("#show-response").innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+  The temperature in Fahrenheit is ${((data[0].main.temp - 273.15) * 9/5 + 32).toFixed(2)} degrees.
+  The weather is ${data[0].weather[0].description}.`;
 }
 
 function handleFormSubmission(e) {
